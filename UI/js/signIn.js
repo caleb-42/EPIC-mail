efd32443c981f787f27867a012ea4377b27f9e6b
ewere <ewiscobaba@gmail.com>
2019-02-27 04:31:29 +0100

    [feature #164234431] Create signIn script files

---
+++ UI/js/signIn.js
@@ -0,0 +1,45 @@
+window.addEventListener('load', (evt) => {
+    $('button').on('click', () => {
+        document.querySelector('.submit').disabled = true;
+        toggleLoader();
+        let email = document.querySelector('input[name="email"]').value;
+        let password = document.querySelector('input[name="password"]').value;
+        const data = {
+            email,
+            password
+        }
+        /* fetch(endpoint, {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json"
+            },
+          body: JSON.stringify(data)
+        }).then((res)=>{
+            console.log(res);
+        }); */
+        
+        window.setTimeout(()=>{/* API call is digused with setTimer function to view loader.gif and REST response message */
+            document.querySelector('.submit').disabled = false;
+            toggleLoader();/* 
+            document.querySelector('.resp').textContent = 'successfully signed in'; */
+            console.log(data);
+        }, 3000);
+    });
+    function toggleLoader(){
+        const loader= document.querySelector('.loader');
+        const resp= document.querySelector('.resp');
+        if(loader.classList.contains('gone')){
+            loader.classList.remove('gone');
+            resp.classList.add('gone');
+        }else{
+            loader.classList.add('gone');
+            resp.classList.remove('gone');
+        }
+    }
+    $('.input-group input').on('focus', (event) => {
+        event.target.parentNode.querySelector('label').classList.add('show');
+    })
+    $('.input-group input').on('blur', (event) => {
+        event.target.parentNode.querySelector('label').classList.remove('show');
+    })
+})
