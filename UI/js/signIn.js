4afbe287c6a4d0f7e202ed8a3073527f9c38e8d5
ewere <ewiscobaba@gmail.com>
2019-02-27 22:03:29 +0100

    [Chore #164281800] Remove all jquery dependencies

--- UI/js/signIn.js
+++ UI/js/signIn.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', (evt) => {
-    $('button').on('click', () => {
+    document.querySelector('button').addEventListener('click', () => {
         document.querySelector('.submit').disabled = true;
         toggleLoader();
         let email = document.querySelector('input[name="email"]').value;
@@ -20,8 +20,8 @@ window.addEventListener('load', (evt) => {
         
         window.setTimeout(()=>{/* API call is digused with setTimer function to view loader.gif and REST response message */
             document.querySelector('.submit').disabled = false;
-            toggleLoader();/* 
-            document.querySelector('.resp').textContent = 'successfully signed in'; */
+            toggleLoader();
+            document.querySelector('.resp').textContent = 'successfully signed in';
             console.log(data);
         }, 3000);
     });
@@ -36,10 +36,14 @@ window.addEventListener('load', (evt) => {
             resp.classList.remove('gone');
         }
     }
-    $('.input-group input').on('focus', (event) => {
-        event.target.parentNode.querySelector('label').classList.add('show');
-    })
-    $('.input-group input').on('blur', (event) => {
-        event.target.parentNode.querySelector('label').classList.remove('show');
+    const inputs = document.querySelectorAll('.input-group input');
+
+    inputs.forEach((input)=>{
+        input.addEventListener('focusin', (evt) => {
+            event.target.parentNode.querySelector('label').classList.add('show');
+        });
+        input.addEventListener('focusout', (evt) => {
+            event.target.parentNode.querySelector('label').classList.remove('show');
+        });
     })
 })
