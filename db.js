c8168ab11be58720b53472c5cfc6914907b71a00
ewere <ewiscobaba@gmail.com>
2019-03-03 19:49:31 +0100

    [Feature #164351385] Make test for signup Endpoint
    Create db.js to handle temporary data persistence.
    
    Install config node module to store Json web token secret key.
    
    Write simple test for create account/sign up API endpoint and
    watch it fail.

---
+++ db.js
@@ -0,0 +1,49 @@
+module.exports = {
+  users: [
+    {
+      id: 0,
+      email: 'admin@gmail.com',
+      firstName: 'admin',
+      lastName: 'user',
+      password: 'admin123',
+      phoneNumber: '2348130439102',
+      isAdmin: true,
+    },
+  ],
+  contacts: [
+    /* {
+      id: 0,
+      email: 'admin@gmail.com',
+      firstName: 'admin',
+      lastName: 'user',
+      password: 'admin123',
+      phoneNumber: '2348130439102',
+      isAdmin: true,
+    }, */
+  ],
+  messages: [
+    /* {
+      id: 0,
+      createdOn: '12-02-2019 09:34:00',
+      subject: 'i just registered',
+      recieverId: 0,
+      senderId: 1,
+      message: 'its so wonderful to be part of this app',
+      parentMessageId: undefined,
+      status: 'sent',
+    }, */
+  ],
+  groups: [
+    /* {
+      id: 0,
+      name: 'coders',
+      createdBy: 'admin',
+    }, */
+  ],
+  groupMembers: [
+    /* {
+      groupId: 0,
+      memberId: 1,
+    }, */
+  ],
+};
--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const config = require('config');
+const winston = require('winston');
+const users = require('./routes/users');
 
 const app = express();
 
 app.use(express.json());
+app.use('/api/v1/users', users);
 
 app.get('/', (req, res) => {
   res.status(200).send('Welcome to EPIC-mail');
 });
 
+if (!config.get('jwtPrivateKey')) {
+  winston.error('Fatal ERROR : jwtPrivateKey is not defined');
+  process.exit(1);
+}
+
 const port = process.env.PORT || 3000;
-const server = app.listen(port, () => console.log(`Listening on port ${port}...`));
+const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
 
 module.exports = server;
