bd985607278677c404f13bf7c6e86bbf0aa51eca
ewere <ewiscobaba@gmail.com>
2019-03-09 09:29:08 +0100

    Chore(auth-mail): test for auth in mails/messages test.
    - write test in mails/messages just to make sure each mail
    endpoint have it as a middleware.
    
    [Chore #164515225]

--- test/routes/mails_test.js
+++ test/routes/mails_test.js
@@ -27,6 +27,16 @@ describe('MAILS API ENDPOINTS', () => {
     expect(messages.body).to.not.have.property('error');
     expect(messages.body.data).to.be.an('array');
   };
+  const noToken = async (endpoint) => {
+    const token = '';
+    const res = await request(server).get(endpoint).set('x-auth-token', token);
+    expect(res.body).to.have.property('status');
+    expect(res.body.status).to.be.equal(401);
+    expect(res.body).to.have.property('error');
+    expect(res.body).to.not.have.property('data');
+    expect(res.body.error).to.be.a('string');
+    expect(res.body.error).to.include('Access denied, no token provided');
+  };
   beforeEach(() => {
     server = require('../../src/index');
     user = {
@@ -42,31 +52,49 @@ describe('MAILS API ENDPOINTS', () => {
     dbHandler.resetDb();
   });
   describe('Get All Mails api/v1/messages/all', () => {
+    it('should not release mails to user with no token', async () => {
+      await noToken('/api/v1/messages/all');
+    });
     it('should release mails for valid user', async () => {
       await validToken('/api/v1/messages/all');
     });
   });
   describe('Get Received Mails api/v1/messages', () => {
+    it('should not release received mails to user with no token', async () => {
+      await noToken('/api/v1/messages');
+    });
     it('should release received mails for valid user', async () => {
       await validToken('/api/v1/messages');
     });
   });
   describe('Get Unread Mails api/v1/messages/unread', () => {
+    it('should not release unread mails to user with no token', async () => {
+      await noToken('/api/v1/messages/unread');
+    });
     it('should release unread mails for valid user', async () => {
       await validToken('/api/v1/messages/unread');
     });
   });
   describe('Get Read Mails api/v1/messages/read', () => {
+    it('should not realease read mails to user with no token', async () => {
+      await noToken('/api/v1/messages/read');
+    });
     it('should realease read mails for valid user', async () => {
       await validToken('/api/v1/messages/read');
     });
   });
   describe('Get Sent Mails api/v1/messages/sent', () => {
+    it('should not realease sent mails to user with no token', async () => {
+      await noToken('/api/v1/messages/sent');
+    });
     it('should realease sent mails for valid user', async () => {
       await validToken('/api/v1/messages/sent');
     });
   });
   describe('Get Draft Mails api/v1/messages/draft', () => {
+    it('should not release draft mails to user with no token', async () => {
+      await noToken('/api/v1/messages/draft');
+    });
     it('should release draft mails for valid user', async () => {
       await validToken('/api/v1/messages/draft');
     });
